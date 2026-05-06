e7f3014cd6a0dddd05809ab20f597353c9906f10
omegafund <omegafund@naver.com>
2026-05-06 18:45:09 +0900

    Add files via upload

--- worker.js
+++ worker.js
@@ -17413,7 +17413,7 @@ function buildSajuSafeCoreV184_5(input) {
       core, interpretation, ohaengAnalysis, yongshin, sixDomain, timeSeries,
       potentialPatterns,  // [V31 #184.7] 특수 잠재력 응답 추가
       daewoon,            // [V31 #186] 대운 8단계 응답 추가
-      _meta: { version: 'V31_186_DAEWOON' }
+      _meta: { version: 'V31_188_PC_OPT' }
     };
   } catch (e) {
     sajuCB_V184_5.record({ error: true });
